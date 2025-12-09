0cb4edd1d565a60552dc30553c6a526f54f5f697
abbeleve <integralisint@outlook.com>
2025-12-10 00:26:59 +0800

    problem219 solved

--- solutions/problem219.py
+++ solutions/problem219.py
@@ -1,3 +1,13 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: list[int], k: int) -> bool:
-        
+        duplicates_map = dict()
+        for index, num in enumerate(nums):
+            if duplicates_map.get(num) is not None:
+                if abs(duplicates_map[num] - index) <= k:
+                    return True
+            duplicates_map[num] = index
+
+        return False
+    
+s = Solution()
+print(s.containsNearbyDuplicate(nums = [2,2], k = 3))
