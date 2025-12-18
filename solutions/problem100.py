2698a6b48d6dfd265f48dd42fb3190fa5409068b
abbeleve <integralisint@outlook.com>
2025-12-19 01:48:56 +0800

    problem100 solved

--- solutions/problem100.py
+++ solutions/problem100.py
@@ -6,21 +6,14 @@ class Solution:
             return False
         if q is None:
             return False
-        stack = self.checkTree(p, [])
-        stack2 = self.checkTree(q, [])
+        stack = self.checkTree(p)
+        stack2 = self.checkTree(q)
         print(stack, stack2)
         if stack == stack2:
             return True
         return False
         
-    def checkTree(self, root, stack):
-        if root.left is None and root.right is None:
-            stack.append(root.val)
-            return stack
-        if root.left:
-            stack = self.checkTree(root.left, stack)
-        if root.right:
-            stack.append(root.val)
-            stack = self.checkTree(root.right, stack)
-        print(stack)
-        return stack
+    def checkTree(self, root):
+        if not root:
+            return [None]
+        return [root.val] + self.checkTree(root.left) + self.checkTree(root.right)
