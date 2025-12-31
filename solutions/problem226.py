5064629e123231621680841e6a7435fbbf9d93f4
abbeleve <integralisint@outlook.com>
2025-12-31 22:28:20 +0800

    problem226 solved

--- solutions/problem226.py
+++ solutions/problem226.py
@@ -1,3 +1,13 @@
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        
+        if root is None:
+            return root
+        if root.left:
+            self.invertTree(root.left)
+        if root.right:
+            self.invertTree(root.right)
+        save = root.left
+        root.left = root.right
+        root.right = save
+
+        return root
