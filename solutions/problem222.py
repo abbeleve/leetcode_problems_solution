a747e66ce58baeae94d79f1d61e86aa61a2c012b
abbeleve <integralisint@outlook.com>
2026-01-08 18:06:49 +0800

    problem6 solved

--- solutions/problem222.py
+++ solutions/problem222.py
@@ -8,14 +8,46 @@ class Solution:
     def countNodes(self, root: Optional[TreeNode]) -> int:
         if root is None:
             return 0
-        amount_of_nodes = self.iterate(root, 0)
+        if root.left is None:
+            return 1
+        if root.right is None:
+            return 2
+        amount_of_nodes = 0
+        left_depth = 0
+        save_root = root
+        while root.left:
+            amount_of_nodes += 2**left_depth
+            root = root.left
+            left_depth += 1
+        print(amount_of_nodes)
+        root = save_root
+        depth = 0
+        while root.left:
+            left_node_ending, right_depth = self.checkRight(root, depth, left_depth)
+            print(left_node_ending, right_depth)
+            if depth == left_depth - 1:
+                if left_node_ending:
+                    return amount_of_nodes + 1
+            if left_depth == right_depth:
+                offset = 1 if left_node_ending else 0
+                amount_of_nodes += 2**(left_depth-depth-offset) + offset
+                return amount_of_nodes
+            root = root.left
+            depth += 1
+
         return amount_of_nodes
 
-    def iterate(self, root, amount_of_nodes):
-        if root is None:
-            return amount_of_nodes
-        amount_of_nodes += 1
-        print(root.val)
-        amount_of_nodes = self.iterate(root.left, amount_of_nodes)
-        amount_of_nodes = self.iterate(root.right, amount_of_nodes)
-        return amount_of_nodes
+    def checkRight(self, root, right_depth, left_depth):
+        # depth_save = right_depth
+        while root.right:
+            root = root.right
+            right_depth += 1
+            if root.left and root.right is None:
+                right_depth += 1
+                return True, right_depth
+        # if left_depth == right_depth:
+        #     return False, right_depth
+        if root.left:
+            right_depth += 1
+            return True, right_depth
+        return False, right_depth
--- solutions/problem6.py
+++ solutions/problem6.py
@@ -1,21 +1,34 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        elem_in_split = 2 * numRows - 2
-        elem_to_add_to_s = elem_in_split - (len(s) % elem_in_split)
-        s += " "*elem_to_add_to_s
-        arr = []
-        for i in range(0, len(s), elem_in_split):
-            arr.append(s[i:i+elem_in_split])
-        print(arr)
-        result_string = ""
-        for i in range(elem_in_split):
-            for string in arr:
-                result_string += string[i]
-        print(result_string)
+        if numRows == 1:
+            return s
+        if numRows == 2:
+            return s[::2] + s[1::2]
+        additional_amount_of_symbols = numRows - 2
+        partition_length = numRows + additional_amount_of_symbols
+        s += (partition_length - (len(s) % partition_length)) * " "
+        newS = []
+        for part_index in range(0, len(s), partition_length):
+            newS.append(s[part_index:part_index+partition_length])
+        left_index, right_index = 0, len(newS[0]) - 1
+        resulting_string = ""
+        while left_index <= right_index:
+            only_left_index = True
+            for i in range(len(newS)):
+                part_newS = newS[i]
+                if left_index == 0 or left_index == numRows - 1:
+                    resulting_string += part_newS[left_index]
+                    only_left_index = True
+                    continue
+                resulting_string += part_newS[left_index] + part_newS[right_index]
+                only_left_index = False
+            if only_left_index:
+                left_index += 1
+            else:
+                left_index += 1
+                right_index -= 1
+        return resulting_string.replace(" ", "")
 
-        result_string = result_string.strip()
-        return result_string
-        
 
 s = Solution()
-print(s.convert("PAYPALISHIRING", 3))
+print(s.convert("ABC", 2))
