ca64d16ac4f9fcbdeb71e1faa42a6202fb9e714a
abbeleve <integralisint@outlook.com>
2026-01-10 20:26:20 +0800

    problem114 solved

--- solutions/problem114.py
+++ solutions/problem114.py
@@ -1,12 +1,18 @@
 class Solution:
     def flatten(self, root: Optional[TreeNode]) -> None:
         stack = self.pre_order_traversal(root, [])
+        save_root = root
+        for node in stack[1:]:
+            root.left = None
+            root.right = node
+            root = root.right
+        return save_root
         
 
     def pre_order_traversal(self, root, stack):
-        if root.left is None and root.right is None:
-            return root.val
-        stack.append(root.val)
-        stack = self.pre_order_traversal(root.left)
-        stack = self.pre_order_traversal(root.right)
+        if root is None:
+            return stack
+        stack.append(root)
+        stack = self.pre_order_traversal(root.left, stack)
+        stack = self.pre_order_traversal(root.right, stack)
         return stack
