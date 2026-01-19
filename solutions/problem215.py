41d1021510b1a38ba8e380a479fdc2a898cbac9a
abbeleve <integralisint@outlook.com>
2026-01-19 20:13:31 +0800

    problem215 solved

--- solutions/problem215.py
+++ solutions/problem215.py
@@ -2,17 +2,14 @@ import heapq
 
 class Solution:
     def findKthLargest(self, nums: list[int], k: int) -> int:
-        heap = heapq.heapify([-i for i in nums])
+        nums = [-1*i for i in nums]
+        heapq.heapify(nums)
+        print(nums)
         if k == 1:
-            return -heap[0]
-        queue = [heap[0]]
-        index = 0
-        max_counter = 1 
-        result = []
-        while len(queue) > 0:
-            node = queue.pop(0)
-            result.append(node)
-            for i in range(index, len(result)):
-                left = 2 * i + 1
-                right = 2 * i + 2
-                queue.append()
+            return -nums[0]
+        for i in range(k):
+            res = heapq.heappop(nums)
+        return -1*res
+    
+s = Solution()
+print(s.findKthLargest([3,2,1,5,6,4], 2))
