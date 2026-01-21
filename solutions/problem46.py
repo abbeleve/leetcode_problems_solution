737ffe109f86984df3075018d79f2076b5dc9c34
abbeleve <integralisint@outlook.com>
2026-01-21 18:27:51 +0800

    problem46 solved

---
+++ solutions/problem46.py
@@ -0,0 +1,25 @@
+#2,34% speed beaten
+class Solution:
+    def permute(self, nums: list[int]) -> list[list[int]]:
+        self.combinations = []
+        self.short_combinations = []
+        self.nums = nums
+        self.backtrack(combination=[])
+        return self.combinations
+
+    def backtrack(self, combination):
+        if len(combination) == len(self.nums):
+            if combination not in self.combinations:
+                self.combinations.append(combination[:])
+        
+        for var in self.nums:
+            if var in combination:
+                continue
+            combination.append(var)
+            self.backtrack(combination)
+            combination.pop()
+
+
+
+s = Solution()
+print(s.permute([1,2,3]))
