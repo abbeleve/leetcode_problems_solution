20205c4803d073de7c7d38d6f41bc4f9f7a5c619
abbeleve <integralisint@outlook.com>
2026-01-21 18:32:17 +0800

    problem46 solved even faster

--- solutions/problem46.py
+++ solutions/problem46.py
@@ -39,5 +39,28 @@ class Solution:
             self.backtrack(combination)
             combination.pop()
 
+#47.78% speed solved
+class Solution:
+    def permute(self, nums: list[int]) -> list[list[int]]:
+        self.combinations = []
+        self.short_combinations = []
+        self.nums = nums
+        self.backtrack(combination=[], hash_combination={})
+        return self.combinations
+
+    def backtrack(self, combination, hash_combination):
+        if len(combination) == len(self.nums):
+            self.combinations.append(combination[:])
+        
+        for var in self.nums:
+            if var in hash_combination:
+                continue
+            combination.append(var)
+            hash_combination[var] = True
+            self.backtrack(combination, hash_combination)
+            combination.pop()
+            hash_combination.pop(var)
+
+
 s = Solution()
 print(s.permute([1,2,3]))
