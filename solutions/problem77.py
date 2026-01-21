7562eb193898307348a22156689afefffde7aecb
abbeleve <integralisint@outlook.com>
2026-01-21 18:05:47 +0800

    problem77 solved

---
+++ solutions/problem77.py
@@ -0,0 +1,24 @@
+class Solution:
+    def combine(self, n: int, k: int) -> list[list[int]]:
+        self.combinations = []
+        self.hash_map_combinations = {}
+        self.backtrack([], n, k)
+        return self.combinations
+
+    def backtrack(self, combination, n, k):
+        if len(combination) == k:
+            self.combinations.append(combination)
+            return
+        if len(combination) > 0:
+            last_elem = combination[-1]
+        else:
+            last_elem = 0
+        for var in range(last_elem + 1, n + 1):
+            new_combination = [i for i in combination]
+            new_combination.append(var)
+            self.backtrack(new_combination, n, k)
+            # if new_combination not in self.combinations:
+            #     self.backtrack(new_combination, n, k)
+    
+s = Solution()
+print(s.combine(4, 2))
