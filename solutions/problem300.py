78dc6ac2e6957a8cc8580062a0825a2ce75c7d15
abbeleve <integralisint@outlook.com>
2026-01-25 20:42:49 +0800

    problem300 solved faster

--- solutions/problem300.py
+++ solutions/problem300.py
@@ -2,18 +2,21 @@ import heapq
 
 class Solution:
     def lengthOfLIS(self, nums: list[int]) -> int:
-        dp = [1]
-        hash_map = {0: [-10**5], 1: [nums[0]]}
+        zero = [-10**5]
+        heapq.heapify(zero)
+        ones = [nums[0]]
+        heapq.heapify(ones)
+        hash_map = {0: zero, 1: ones}
         maximum_subsequence_length = 1
         for index, num in enumerate(nums[1:]):
             for subsequence_length in range(maximum_subsequence_length, -1, -1):
-                subsequence_num = min(hash_map[subsequence_length])
+                subsequence_num = hash_map[subsequence_length][0]
                 if num > subsequence_num:
                     if subsequence_length + 1 not in hash_map:
                         hash_map[subsequence_length + 1] = [num]
                         maximum_subsequence_length += 1
                     else:
-                        hash_map[subsequence_length + 1].append(num)
+                        heapq.heappush(hash_map[subsequence_length + 1], num)
                     break
         return maximum_subsequence_length
 
