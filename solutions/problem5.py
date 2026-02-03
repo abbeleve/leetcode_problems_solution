bd2d6a7ff53dc15fa8f3991dd411d7ba5f91f333
abbeleve <integralisint@outlook.com>
2026-02-03 21:15:18 +0800

    problem5 bad solution

--- solutions/problem5.py
+++ solutions/problem5.py
@@ -1,3 +1,15 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        
+        palindrom = [0, 1]
+        for palindrome_length in range(2, len(s) + 1):
+            for i in range(0, len(s) - palindrome_length + 1):
+                if self.checkIfPalindrome(s[i:i+palindrome_length]):
+                    palindrom = [i, i+palindrome_length]
+                    break
+        return s[palindrom[0]:palindrom[1]]
+
+    def checkIfPalindrome(self, s: str) -> bool:
+        return s == s[::-1]
+    
+s = Solution()
+print(s.longestPalindrome(s = "bb"))
