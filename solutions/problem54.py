98e285d679412ab81d090feca733bd1298d9cdd3
abbeleve <integralisint@outlook.com>
2026-02-07 19:05:40 +0800

    problem54 solved

--- solutions/problem54.py
+++ solutions/problem54.py
@@ -1,3 +1,60 @@
 class Solution:
     def spiralOrder(self, matrix: list[list[int]]) -> list[int]:
-        
+        direction = 0
+        result = []
+        while True:
+            if len(matrix) == 1:
+                if direction == 0:
+                    result.extend(matrix[0])
+                    return result
+                elif direction == 1:
+                    if len(matrix[0]) == 1:
+                        result.append(matrix[0][0])
+                        return result
+                    result.append(matrix[0][-1])
+                    matrix[0].pop()
+                elif direction == 2:
+                    result.extend(matrix[0][::-1])
+                    return result
+                elif direction == 3:
+                    if len(matrix[0]) == 1:
+                        result.append(matrix[0][0])
+                        return result
+                    result.append(matrix[0][0])
+                    matrix[0].pop(0)
+                continue
+            elif len(matrix[0]) == 1:
+                if direction == 0:
+                    result.append(matrix[0][0])
+                    matrix.pop(0)
+                elif direction == 1:
+                    result.extend([row[0] for row in matrix])
+                    return result
+                elif direction == 2:
+                    result.append(matrix[-1][0])
+                    matrix.pop()
+                elif direction == 3:
+                    add = [row[0] for row in matrix]
+                    add.reverse()
+                    result.extend(add)
+                    return result
+            else:
+                if direction == 0:
+                    result.extend(matrix[0])
+                    matrix = matrix[1:]
+                elif direction == 1:
+                    result.extend([row[-1] for row in matrix])
+                    matrix = [row[:-1] for row in matrix]
+                elif direction == 2:
+                    result.extend(matrix[-1][::-1])
+                    matrix = matrix[:-1]
+                elif direction == 3:
+                    add = [row[0] for row in matrix]
+                    add.reverse()
+                    result.extend(add)
+                    matrix = [row[1:] for row in matrix]
+            direction += 1
+            direction = direction % 4
+
+s = Solution()
+print(s.spiralOrder(matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]))
--- solutions/problem76.py
+++ solutions/problem76.py
@@ -1,29 +1,43 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        hash_map = {}
-        amount_of_letters = {}
-        for string in t:
-            if string not in hash_map:
-                hash_map[string] = []
-                amount_of_letters[string] = 1
+        left_pointer = 0
+        right_pointer = -1
+        result_left_pointer = 0
+        result_right_pointer = 10**5
+        num_of_letters = {letter:t.count(letter) for letter in t}
+        first_time_flag = False
+
+        while right_pointer < len(s) - 1:
+            moving_right = False
+            for letter in t:
+                if num_of_letters[letter] > 0:
+                    if s[left_pointer] in num_of_letters and num_of_letters[s[left_pointer]] == 0:
+                        moving_right = True
+                    else:
+                        first_time_flag = True
+                        break
+            if moving_right:
+                right_pointer += 1
+                if s[right_pointer] in num_of_letters:
+                    num_of_letters[s[right_pointer]] -= 1
             else:
-                # hash_map[string]
-                amount_of_letters[string] += 1
-
-        for index, string in enumerate(s):
-            if string in hash_map:
-                hash_map[string].append(index)
-
-        left_index = len(s)
-        right_index = 0        
-        for string in t:
-            left_index = min(left_index, hash_map[string][0])
-            right_index = max(right_index, hash_map[string][0])
-        # CREATING BEST APPROACH SO LEFT INDEX IS MOST LEFT AND RIGHT INDEX IS MOST LEFT TOO
-
-        while right_index < len(s) - 1:
-            while True:
-                
+                if s[left_pointer] in num_of_letters:
+                    num_of_letters[s[left_pointer]] += 1
+                left_pointer += 1
+                if first_time_flag:
+                    if result_right_pointer - result_left_pointer < right_pointer - left_pointer:
+                        result_right_pointer = right_pointer
+                        result_left_pointer = left_pointer
+        while left_pointer < right_pointer:
+            if s[left_pointer] in num_of_letters:
+                if num_of_letters[s[left_pointer]] == 0:
+                    break
+                num_of_letters[s[left_pointer]] += 1
+            left_pointer += 1
+        for letter in t:
+            if num_of_letters[letter] > 0:
+                return ""
+        return s[left_pointer:right_pointer + 1]
 
 s = Solution()
-print(s.minWindow(s = "ADOBECODEBANC", t = "ABC"))
+print(s.minWindow(s = "ab", t = "a"))
