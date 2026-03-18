ebb73f1220f7e9ce5d304707ed97454d3bfd64d8
abbeleve <integralisint@outlook.com>
2026-03-18 23:54:19 +0800

    problem22 solved

--- solutions/problem22.py
+++ solutions/problem22.py
@@ -1,11 +1,25 @@
 class Solution:
     def generateParenthesis(self, n: int) -> list[str]:
-        self.combinations = []
-        self.hash_combination = {}
+        self.combinations = set()
         self.n = n
-
+        self.backtrack([])
+        return list(self.combinations)
+    
     def backtrack(self, combination):
-        if len(combination) == 2*self.n:
-            self.combinations.append(combination)
+        if self.n == combination.count("(") == combination.count(")"):
+            self.combinations.add("".join(combination))
+            return
         
-        for input_place in range()
+        for var in ("(", ")"):
+            combination.append(var)
+            if combination.count("(") < combination.count(")"):
+                combination.pop()
+                continue
+            if combination.count("(") > self.n:
+                combination.pop()
+                continue
+            self.backtrack(combination)
+            combination.pop()
+
+s = Solution()
+print(s.generateParenthesis(3))
