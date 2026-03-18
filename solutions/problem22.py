46387e135d57534ee550c1fde013eb3348ca9df8
abbeleve <integralisint@outlook.com>
2026-03-19 00:01:06 +0800

    problem22 solved faster

--- solutions/problem22.py
+++ solutions/problem22.py
@@ -2,24 +2,32 @@ class Solution:
     def generateParenthesis(self, n: int) -> list[str]:
         self.combinations = set()
         self.n = n
-        self.backtrack([])
+        self.backtrack([], 0, 0)
         return list(self.combinations)
     
-    def backtrack(self, combination):
-        if self.n == combination.count("(") == combination.count(")"):
+    def backtrack(self, combination, amount_of_left_brackets, amount_of_right_brackets):
+        if self.n == amount_of_left_brackets == amount_of_right_brackets:
             self.combinations.add("".join(combination))
             return
         
         for var in ("(", ")"):
+            if var == "(":
+                amount_of_left_brackets += 1
+            else:
+                amount_of_right_brackets += 1
+            if amount_of_left_brackets < amount_of_right_brackets:
+                amount_of_right_brackets -= 1
+                continue
+            if amount_of_left_brackets > self.n:
+                amount_of_left_brackets -= 1
+                continue
             combination.append(var)
-            if combination.count("(") < combination.count(")"):
-                combination.pop()
-                continue
-            if combination.count("(") > self.n:
-                combination.pop()
-                continue
-            self.backtrack(combination)
+            self.backtrack(combination, amount_of_left_brackets, amount_of_right_brackets)
             combination.pop()
+            if var == "(":
+                amount_of_left_brackets -= 1
+            else:
+                amount_of_right_brackets -= 1
 
 s = Solution()
 print(s.generateParenthesis(3))
