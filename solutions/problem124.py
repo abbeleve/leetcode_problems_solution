c6724a421e1666abacd94986e46a941dc1329121
abbeleve <integralisint@outlook.com>
2026-04-16 16:46:05 +0800

    problem124 solved

--- solutions/problem124.py
+++ solutions/problem124.py
@@ -6,15 +6,23 @@
 #         self.right = right
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        self.path(root)
+        self.maxPath = -10**13
+        self.pathCounter(root)
+        return self.maxPath
 
-    def path(self, root):
-        left_sum, right_sum = None, None
-        if root.left:
-            left_sum = self.path(root.left)
-        if root.right:
-            right_sum = self.path(root.right)
-        if left_sum and right_sum:
-            
+    def pathCounter(self, root):
         if root.left is None and root.right is None:
+            self.maxPath = max(self.maxPath, root.val)
             return root.val
+        max_local_sum = root.val
+        if root.left:
+            left_counter = self.pathCounter(root.left)
+            max_local_sum = max(max_local_sum, root.val + left_counter)
+        if root.right:
+            right_counter = self.pathCounter(root.right)
+            max_local_sum = max(max_local_sum, root.val + right_counter)
+        if root.left and root.right:
+            max_sum = max(left_counter + root.val, right_counter + root.val, left_counter + right_counter + root.val, root.val)
+            self.maxPath = max(self.maxPath, max_sum)
+        self.maxPath = max(self.maxPath, max_local_sum)
+        return max_local_sum
