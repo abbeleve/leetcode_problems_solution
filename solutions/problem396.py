3d4c0e2dc779a98d5a4349569974f1dd511fc6a9
abbeleve <integralisint@outlook.com>
2026-05-08 12:24:37 +0800

    problem396 solved

--- solutions/problem396.py
+++ solutions/problem396.py
@@ -1,3 +1,13 @@
 class Solution:
     def maxRotateFunction(self, nums: list[int]) -> int:
-        
+        sums = sum(nums) - nums[0]
+        clockwise_sums = sum([i*nums[i] for i in range(len(nums))])
+        result = max(float('-inf'), clockwise_sums)
+        for i in range(1, len(nums)):
+            clockwise_sums = clockwise_sums - sums + (len(nums) - 1) * nums[i - 1]
+            sums = sums - nums[i] + nums[i - 1]
+            result = max(result, clockwise_sums)
+        return result
+    
+s = Solution()
+print(s.maxRotateFunction(nums = [4,3,2,6]))
