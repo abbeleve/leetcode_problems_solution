cc78976fd3678c55727702384eba75b19a4c2cf0
abbeleve <integralisint@outlook.com>
2026-06-21 18:21:08 +0800

    problem99 print fix

--- solutions/problem99.py
+++ solutions/problem99.py
@@ -13,8 +13,6 @@ class Solution:
             return None
         self.tree_traverse = []
         self.traverse(root)
-        for i in self.tree_traverse:
-            print(i.val)
         first_deceiver, second_deceiver = None, None
         for index in range(len(self.tree_traverse) - 1):
             if self.tree_traverse[index].val < self.tree_traverse[index + 1].val:
@@ -23,15 +21,12 @@ class Solution:
                 first_deceiver = index
             else:
                 second_deceiver = index + 1
-        print(first_deceiver, second_deceiver)
         if first_deceiver is not None and second_deceiver is not None:
             if first_deceiver == second_deceiver - 1:
                 self.tree_traverse[first_deceiver].val, self.tree_traverse[second_deceiver + 1].val = self.tree_traverse[second_deceiver + 1].val, self.tree_traverse[first_deceiver].val
-                print(root.val)
                 return root
             self.tree_traverse[first_deceiver].val, self.tree_traverse[second_deceiver].val = self.tree_traverse[second_deceiver].val, self.tree_traverse[first_deceiver].val
             return root
-        # print(first_deceiver, second_deceiver)
         second_deceiver = first_deceiver + 1
         self.tree_traverse[first_deceiver].val, self.tree_traverse[second_deceiver].val = self.tree_traverse[second_deceiver].val, self.tree_traverse[first_deceiver].val
         return root
