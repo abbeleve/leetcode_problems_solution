a5fa2858ace7f7a540fcab8c0aa3f2bb9ee24a8c
abbeleve <integralisint@outlook.com>
2026-07-02 21:58:08 +0800

    problem435 solved

--- solutions/problem435.py
+++ solutions/problem435.py
@@ -1,5 +1,20 @@
 class Solution:
     def eraseOverlapIntervals(self, intervals: list[list[int]]) -> int:
         intervals.sort(key=lambda x: (x[0], x[1]))
-        for i in range(len(intervals)):
-            
+        i = 0
+        amount_of_deletions = 0
+        while i < len(intervals) - 1:
+            if intervals[i + 1][1] <= intervals[i][1]:
+                intervals.pop(i)
+                amount_of_deletions += 1
+                continue
+            if intervals[i + 1][0] < intervals[i][1]:
+                intervals.pop(i + 1)
+                amount_of_deletions += 1
+                continue
+            if intervals[i][1] <= intervals[i + 1][0]:
+                i += 1
+        return amount_of_deletions
+
+s = Solution()
+print(s.eraseOverlapIntervals([[0,2],[1,3],[2,4],[3,5],[4,6]]))
