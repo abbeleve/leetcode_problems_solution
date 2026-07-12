21752ce17e7b3c42b3d3b083200e6cbcee01b86f
abbeleve <integralisint@outlook.com>
2026-07-12 19:18:38 +0800

    problem96 solved

--- solutions/problem96.py
+++ solutions/problem96.py
@@ -1,5 +1,15 @@
 class Solution:
     def numTrees(self, n: int) -> int:
-        
-    def process(self, left_branch, right_branch):
-        self.process(left_branch - 1, )
+        dp = [1] * (n + 1)
+        dp[0] = 0
+        for i in range(2, n + 1):
+            combination_amount = 0
+            for j in range(1, i + 1):
+                left_subtree = max(1, dp[j - 1])
+                right_subtree = max(1, dp[i - j])
+                combination_amount += left_subtree * right_subtree
+            dp[i] = combination_amount
+        return dp[-1]
+
+s = Solution()
+print(s.numTrees(4))
