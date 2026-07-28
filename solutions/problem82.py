9d73a58cd4ba44b82bf292b89599fb377038c52a
abbeleve <integralisint@outlook.com>
2026-07-28 18:31:35 +0800

    problem82 solved

--- solutions/problem82.py
+++ solutions/problem82.py
@@ -5,16 +5,40 @@
 #         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None:
+            return None
+        start_node = ListNode(None, head)
+        end_node = ListNode(None, None)
         save_head = head
-        head_next = head.next
-        hash_duplicates = {}
-        while head_next is not None:
-            if head.val == head_next.val:
-                head_next = head_next.next
-                hash_duplicates[head.val] = True
-            else:
-                head = head_next
-                head_next = head_next.next
-        head.next = None
+        while head.next is not None:
+            head = head.next
+        head.next = end_node
+        head = save_head
+
+        hash_map = {}
+        while head is not None:
+            hash_map[head.val] = hash_map.get(head.val, 0) + 1
+            head = head.next
+        head = save_head
+
+        keys = list(hash_map.keys())
+        list_for_deletion = set()
+        for key in keys:
+            if hash_map[key] > 1:
+                list_for_deletion.add(key)
         
+        save_head = None
+        left_node, middle_node, right_node = start_node, head, head.next
+        while right_node is not None:
+            if middle_node.val in list_for_deletion:
+                left_node.next = right_node
+                middle_node = middle_node.next
+                right_node = right_node.next
+            else:
+                if save_head is None:
+                    save_head = middle_node
+                left_node = left_node.next
+                middle_node = middle_node.next
+                right_node = right_node.next
+        left_node.next = None
         return save_head
