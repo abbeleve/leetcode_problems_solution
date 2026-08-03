92d61cf684a4c0c5737e40f6b0dd02c3d56e8848
Andrey <integralisint@outlook.com>
2026-08-03 23:37:31 +0800

    problem217 solved

--- solutions/problem456.py
+++ solutions/problem456.py
@@ -3,16 +3,7 @@ from collections import deque
 class Solution:
     def find132pattern(self, nums: list[int]) -> bool:
         stack = deque()
-        deletion_happened = False
-        for index in range(len(nums)):
-            while stack and stack[-1] > nums[index]:
-                stack.pop()
-                deletion_happened = True
-            if deletion_happened and stack:
-                return True
-            stack.append(nums[index])
-            deletion_happened = False
-        return False
+        
 
 s = Solution()
 print(s.find132pattern(nums = [3,5,0,3,4]))
