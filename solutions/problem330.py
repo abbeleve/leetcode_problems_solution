f8e6efd54e4d65127af5ef980e243a90a9382c7d
abbeleve <integralisint@outlook.com>
2026-08-25 20:50:12 +0800

    problem330 solved

--- solutions/problem330.py
+++ solutions/problem330.py
@@ -1,25 +1,34 @@
-import math
-
 class Solution:
     def minPatches(self, nums: list[int], n: int) -> int:
+        num_patches = 0
         if nums[0] != 1:
             nums.insert(0, 1)
-        num_patches = 0
-        diff = 0
-        sums = [nums[0]]
-        for i in range(1, len(nums)):
-            sums.append(sums[i - 1] + nums[i])
-        for i in range(len(sums) - 1):
-            prefix_sum = sums[i] + diff
+            num_patches += 1
+        i = 0
+        while i < len(nums):
+            if nums[i] < n:
+                i += 1
+            else:
+                break
+        nums.insert(i, n)
+        nums = nums[0:i + 1]
+        prefix_sum = 0
+        for i in range(len(nums) - 1):
+            prefix_sum += nums[i]
+            if i == len(nums) - 2:
+                if prefix_sum >= nums[i + 1]:
+                    continue
+                while prefix_sum < nums[i + 1]:
+                    new_patch = prefix_sum + 1
+                    prefix_sum += new_patch
+                    num_patches += 1
             if prefix_sum >= nums[i + 1] - 1:
                 continue
-            diff = nums[i + 1] - 1 - prefix_sum
-            save_diff = diff
-            while save_diff > 0:
-                save_diff -= prefix_sum + 1
+            while prefix_sum < nums[i + 1] - 1:
+                new_patch = prefix_sum + 1
+                prefix_sum += new_patch
                 num_patches += 1
-        print(diff)
         return num_patches
-    
+
 s = Solution()
 print(s.minPatches(nums = [1,12,15], n = 43))
